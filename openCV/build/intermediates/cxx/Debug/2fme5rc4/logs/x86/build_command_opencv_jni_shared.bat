d6b178dccf5fd8cf06fda9631b2517f9e9c9fbc8
Jean <jeancabrera2003@gmail.com>
2024-08-08 00:32:07 +0800

    added gitignore/ changed gradle version to 17

--- openCV/build/intermediates/cxx/Debug/2fme5rc4/logs/x86/build_command_opencv_jni_shared.bat
+++ openCV/build/intermediates/cxx/Debug/2fme5rc4/logs/x86/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\user\\Desktop\\thesis\\BasicCamera2App-main\\openCV\\.cxx\\Debug\\2fme5rc4\\x86" ^
+  "C:\\Users\\user\\Desktop\\thesis\\try\\Eagle-Eye-4.0\\openCV\\.cxx\\Debug\\2fme5rc4\\x86" ^
   opencv_jni_shared
