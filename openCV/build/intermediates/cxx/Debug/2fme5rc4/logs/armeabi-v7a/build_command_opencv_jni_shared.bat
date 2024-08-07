7d608b3c88bbc2aa4856b5e6a8ce732c117c4b5b
Jean Cabrera <119938233+Skillial@users.noreply.github.com>
2024-08-08 00:33:46 +0800

    Delete openCV/build

--- openCV/build/intermediates/cxx/Debug/2fme5rc4/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\user\\Desktop\\thesis\\BasicCamera2App-main\\openCV\\.cxx\\Debug\\2fme5rc4\\armeabi-v7a" ^
-  opencv_jni_shared
